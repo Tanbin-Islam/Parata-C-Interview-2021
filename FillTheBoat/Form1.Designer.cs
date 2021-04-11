0c843dcfcaa9568ecdac51e35d3e09b70d1142bb
Tanbin-Islam <tanbinislam1996@gmail.com>
2021-04-12 02:04:17 +0600

    Fill the boat

--- FillTheBoat/Form1.Designer.cs
+++ FillTheBoat/Form1.Designer.cs
@@ -29,13 +29,13 @@ namespace FillTheBoat
         /// </summary>
         private void InitializeComponent()
         {
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.sldMC = new System.Windows.Forms.HScrollBar();
             this.label1 = new System.Windows.Forms.Label();
-            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.sldT = new System.Windows.Forms.HScrollBar();
             this.label2 = new System.Windows.Forms.Label();
-            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.sldC = new System.Windows.Forms.HScrollBar();
             this.label3 = new System.Windows.Forms.Label();
-            this.hScrollBar4 = new System.Windows.Forms.HScrollBar();
+            this.sldTC = new System.Windows.Forms.HScrollBar();
             this.label4 = new System.Windows.Forms.Label();
             this.lblMCCount = new System.Windows.Forms.Label();
             this.lblTCount = new System.Windows.Forms.Label();
@@ -43,15 +43,18 @@ namespace FillTheBoat
             this.lblTCCount = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // hScrollBar1
+            // sldMC
             // 
-            this.hScrollBar1.Location = new System.Drawing.Point(67, 251);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(212, 17);
-            this.hScrollBar1.TabIndex = 0;
+            this.sldMC.Location = new System.Drawing.Point(70, 251);
+            this.sldMC.Name = "sldMC";
+            this.sldMC.Size = new System.Drawing.Size(212, 17);
+            this.sldMC.TabIndex = 0;
+            this.sldMC.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sldMC_Scroll);
             // 
             // label1
             // 
@@ -62,12 +65,13 @@ namespace FillTheBoat
             this.label1.Text = "Motor Cycle(3 units)";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // hScrollBar2
+            // sldT
             // 
-            this.hScrollBar2.Location = new System.Drawing.Point(452, 251);
-            this.hScrollBar2.Name = "hScrollBar2";
-            this.hScrollBar2.Size = new System.Drawing.Size(212, 17);
-            this.hScrollBar2.TabIndex = 0;
+            this.sldT.Location = new System.Drawing.Point(452, 251);
+            this.sldT.Name = "sldT";
+            this.sldT.Size = new System.Drawing.Size(212, 17);
+            this.sldT.TabIndex = 0;
+            this.sldT.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sldT_Scroll);
             // 
             // label2
             // 
@@ -78,12 +82,13 @@ namespace FillTheBoat
             this.label2.Text = "Trucks(11 units)";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // hScrollBar3
+            // sldC
             // 
-            this.hScrollBar3.Location = new System.Drawing.Point(67, 344);
-            this.hScrollBar3.Name = "hScrollBar3";
-            this.hScrollBar3.Size = new System.Drawing.Size(212, 17);
-            this.hScrollBar3.TabIndex = 0;
+            this.sldC.Location = new System.Drawing.Point(67, 344);
+            this.sldC.Name = "sldC";
+            this.sldC.Size = new System.Drawing.Size(212, 17);
+            this.sldC.TabIndex = 0;
+            this.sldC.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sldC_Scroll);
             // 
             // label3
             // 
@@ -94,12 +99,13 @@ namespace FillTheBoat
             this.label3.Text = "Car(5 units)";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // hScrollBar4
+            // sldTC
             // 
-            this.hScrollBar4.Location = new System.Drawing.Point(452, 344);
-            this.hScrollBar4.Name = "hScrollBar4";
-            this.hScrollBar4.Size = new System.Drawing.Size(212, 17);
-            this.hScrollBar4.TabIndex = 0;
+            this.sldTC.Location = new System.Drawing.Point(452, 344);
+            this.sldTC.Name = "sldTC";
+            this.sldTC.Size = new System.Drawing.Size(212, 17);
+            this.sldTC.TabIndex = 0;
+            this.sldTC.Scroll += new System.Windows.Forms.ScrollEventHandler(this.sldTC_Scroll);
             // 
             // label4
             // 
@@ -115,36 +121,36 @@ namespace FillTheBoat
             this.lblMCCount.AutoSize = true;
             this.lblMCCount.Location = new System.Drawing.Point(67, 223);
             this.lblMCCount.Name = "lblMCCount";
-            this.lblMCCount.Size = new System.Drawing.Size(35, 13);
+            this.lblMCCount.Size = new System.Drawing.Size(13, 13);
             this.lblMCCount.TabIndex = 2;
-            this.lblMCCount.Text = "label5";
+            this.lblMCCount.Text = "0";
             // 
             // lblTCount
             // 
             this.lblTCount.AutoSize = true;
             this.lblTCount.Location = new System.Drawing.Point(449, 223);
             this.lblTCount.Name = "lblTCount";
-            this.lblTCount.Size = new System.Drawing.Size(35, 13);
+            this.lblTCount.Size = new System.Drawing.Size(13, 13);
             this.lblTCount.TabIndex = 2;
-            this.lblTCount.Text = "label5";
+            this.lblTCount.Text = "0";
             // 
             // lblCCount
             // 
             this.lblCCount.AutoSize = true;
             this.lblCCount.Location = new System.Drawing.Point(67, 321);
             this.lblCCount.Name = "lblCCount";
-            this.lblCCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCCount.Size = new System.Drawing.Size(13, 13);
             this.lblCCount.TabIndex = 2;
-            this.lblCCount.Text = "label5";
+            this.lblCCount.Text = "0";
             // 
             // lblTCCount
             // 
             this.lblTCCount.AutoSize = true;
             this.lblTCCount.Location = new System.Drawing.Point(449, 321);
             this.lblTCCount.Name = "lblTCCount";
-            this.lblTCCount.Size = new System.Drawing.Size(35, 13);
+            this.lblTCCount.Size = new System.Drawing.Size(13, 13);
             this.lblTCCount.TabIndex = 2;
-            this.lblTCCount.Text = "label5";
+            this.lblTCCount.Text = "0";
             // 
             // pictureBox1
             // 
@@ -157,9 +163,29 @@ namespace FillTheBoat
             // progressBar1
             // 
             this.progressBar1.Location = new System.Drawing.Point(330, 35);
+            this.progressBar1.Maximum = 500;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(331, 52);
             this.progressBar1.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(330, 108);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(152, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Capacity : 239, current load : 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "New Boat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -167,6 +193,8 @@ namespace FillTheBoat
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(828, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblTCCount);
@@ -177,10 +205,10 @@ namespace FillTheBoat
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.hScrollBar4);
-            this.Controls.Add(this.hScrollBar3);
-            this.Controls.Add(this.hScrollBar2);
-            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.sldTC);
+            this.Controls.Add(this.sldC);
+            this.Controls.Add(this.sldT);
+            this.Controls.Add(this.sldMC);
             this.Name = "Form1";
             this.Text = "Fill The boat";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -191,13 +219,13 @@ namespace FillTheBoat
 
         #endregion
 
-        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar sldMC;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar sldT;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar sldC;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.HScrollBar hScrollBar4;
+        private System.Windows.Forms.HScrollBar sldTC;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblMCCount;
         private System.Windows.Forms.Label lblTCount;
@@ -205,6 +233,8 @@ namespace FillTheBoat
         private System.Windows.Forms.Label lblTCCount;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- FillTheBoat/Form1.cs
+++ FillTheBoat/Form1.cs
@@ -16,5 +16,80 @@ namespace FillTheBoat
         {
             InitializeComponent();
         }
+        int totalLoad = 0;
+        int capacity = 239;
+        int truckload = 0;
+        int carload = 0;
+        int mcload = 0;
+        int traincarload = 0;
+        int truckvalue = 11;
+        int carvalue = 5;
+        int mcvalue = 3;
+        int traincarvalue = 17;
+
+        private void sldMC_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblMCCount.Text = sldMC.Value.ToString();
+            calculate();
+        }
+
+        private void sldT_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblTCount.Text = sldT.Value.ToString();
+            calculate();
+        }
+
+        private void sldC_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblCCount.Text = sldC.Value.ToString();
+            calculate();
+        }
+
+        private void sldTC_Scroll(object sender, ScrollEventArgs e)
+        {
+            lblTCCount.Text = sldTC.Value.ToString();
+            calculate();
+        }
+
+        private void calculate()
+        {
+            
+            truckload = truckvalue* sldT.Value;
+            carload = carvalue* sldC.Value;
+            mcload = mcvalue* sldMC.Value;
+            traincarload = traincarvalue* sldTC.Value;
+            totalLoad = truckload + carload + mcload + traincarload;
+            lblResult.Text = "Capacity : 239, current load : " + totalLoad.ToString();
+            progressBar1.Value = (totalLoad >= progressBar1.Maximum)? progressBar1.Maximum: totalLoad;
+            if (totalLoad<capacity)
+            {
+                progressBar1.ForeColor = Color.Brown;
+                progressBar1.Style= ProgressBarStyle.Continuous;
+            }
+            else if (totalLoad == capacity)
+            {
+                progressBar1.ForeColor = Color.Green;
+                progressBar1.Style= ProgressBarStyle.Continuous;
+            }
+            else
+            {
+                progressBar1.ForeColor = Color.Red;
+                progressBar1.Style= ProgressBarStyle.Continuous;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sldMC.Value=0;
+            sldT.Value=0;
+            sldC.Value=0;
+            sldTC.Value=0;
+            lblTCCount.Text = sldTC.Value.ToString();
+            lblCCount.Text = sldC.Value.ToString();
+            lblMCCount.Text = sldMC.Value.ToString();
+            lblTCount.Text = sldT.Value.ToString();
+
+            calculate();
+        }
     }
 }
